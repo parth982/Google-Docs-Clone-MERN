2699d6309780afbead8e810879c9807959ae05f9
Parth_Ghatge <parthghatge982@gmail.com>
2023-08-24 21:31:35 +0530

    Seetting-up: Quill Editor + server socket.io

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TextEditor from "./TextEditor";
+
+const App = () => {
+  return <TextEditor />;
+};
+
+export default App;
---
+++ client/src/TextEditor.jsx
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
+const TextEditor = () => {
+  useEffect(() => {
+    const socket = io("http://localhost:3001");
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
+};
+
+export default TextEditor;
